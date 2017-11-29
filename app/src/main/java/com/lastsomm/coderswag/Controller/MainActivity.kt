a9bac971db73a907c306935c3a47923ea09c7901
JSlast <33830253+JSlast@users.noreply.github.com>
2017-11-29 21:04:15 +1100

    Lesson 3

--- app/src/main/java/com/lastsomm/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/lastsomm/coderswag/Controller/MainActivity.kt
@@ -2,8 +2,10 @@ package com.lastsomm.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
-import com.lastsomm.coderswag.Adapters.CategoryAdapter
+import android.widget.LinearLayout
+import com.lastsomm.coderswag.Adapters.CategoryRecycleAdapter
 import com.lastsomm.coderswag.Model.Category
 import com.lastsomm.coderswag.R
 import com.lastsomm.coderswag.Services.DataService
@@ -11,14 +13,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this,DataService.categories)
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager (this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
     }
 
 }
